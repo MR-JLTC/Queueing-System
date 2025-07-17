dd8c1a3f0782198b42aaa79e6dbc85e57fa0d85d
MR-JLTC <jhonlloydtcruz4@gmail.com>
2025-07-17 17:53:38 +0800

    ambot

--- frontend/system_ui/src/routes/ProtectedRoute.jsx
+++ frontend/system_ui/src/routes/ProtectedRoute.jsx
@@ -4,7 +4,6 @@ import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
   const isAuthenticated = localStorage.getItem("isLoggedIn") === "true"; // or your token logic
-
   return isAuthenticated ? children : <Navigate to="/login" />;
 };
 
